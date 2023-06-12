a45c68fc9d0d65ef1aea38667b3b1c6674f43cbc
Patrickroam <patrick.roan96@gmail.com>
2023-06-12 05:39:12 -0300

    Refazendo HTML E CSS, e iniciando JS

--- assets/javascript/javaScript.js
+++ assets/javascript/javaScript.js
@@ -1,59 +1,16 @@
+function iniciaApp() {
+    carregaMensagens();
+    setInterval(carregaMensagens, 3000);
 
-function pegaDadosAxios(){
-    const promessa = axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/messages');
-    promessa.then(pegaMenssagem);
-}
-pegaDadosAxios()
+    perguntaNome();
 
 
-function pegaMenssagem(resposta){
-    const mensagem = resposta.data;
-
-    const bodyMain = document.querySelector(".bodyMain");
-        
-        for(let i = 0; i < mensagem.length; i++){
-        
-            bodyMain.innerHTML += `
-            <section class="directMessage">
-                <P class="text">
-                    <span class="hour">${mensagem[i].time}</span>
-                    <strong class="name">${mensagem[i].from}</strong> para<strong class="to"> ${mensagem[i].to}</strong>: ${mensagem[i].text}
-                </P>
-            </section>`;
-
-            bodyMain.innerHTML += `
-            <section class="status-Online">
-                <P class="text">
-                    <span class="hour">${mensagem[i].time}</span>
-                    <strong class="name">${mensagem[i].from}o</strong> entra na sala...
-                 </P>
-            </section>`;
-
-            bodyMain.innerHTML += `
-            <section class="massage-Everyone">
-                <P class="text">
-                    <span class="hour">${mensagem[i].time}</span>
-                    <strong class="name">${mensagem[i].from}</strong> para<strong class="to"> ${mensagem[i].to}</strong>: Bom dia
-                </P>
-            </section>`;
-         
-            bodyMain.innerHTML += `
-            <section class="status-Offline">
-                <P class="text">
-                    <span class="hour">${mensagem[i].time}</span>
-                    <strong class="name">${mensagem[i].from}</strong> saiu da sala...
-                </P>
-            </section>`;   
-
-            bodyMain.innerHTML += `
-            <section class="reservedMessage">
-                <P class="text">
-                    <span class="hour">${mensagem[i].time}</span>
-                 <strong class="name">${mensagem[i].from}</strong> reservadamente <strong class="to">${mensagem[i].from}</strong>: Oi gatinha quer tc? KKKKKKKKKKKKKKKK
-                </P>
-            </section>`;   
-        }
-   
 }
 
+function carregaMensagens() {
+    const promessa = axio.get("https://mock-api.driven.com.br/api/v4/uol/messages")
 
+    promessa.then((resposta) => {
+        const mensagem = resposta.data;
+    })
+}
