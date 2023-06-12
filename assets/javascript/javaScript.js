18fbfa2a0e68ce341be4e836b394e4e760531030
Patrickroam <patrick.roan96@gmail.com>
2023-06-12 17:30:52 -0300

    Criando funções: iniciaApp, mensagem e scrollarAteAUltimaMensagem

--- assets/javascript/javaScript.js
+++ assets/javascript/javaScript.js
@@ -1,3 +1,6 @@
+let ultimaMensagem;
+let nomeDeUsuario;
+
 function iniciaApp() {
     carregaMensagens();
     setInterval(carregaMensagens, 3000);
@@ -11,6 +14,60 @@ function carregaMensagens() {
     const promessa = axio.get("https://mock-api.driven.com.br/api/v4/uol/messages")
 
     promessa.then((resposta) => {
-        const mensagem = resposta.data;
-    })
+        const mensagens = resposta.data;
+
+        const ulMensagens = document.querySelector(".mensagens-container");
+        ulMensagens.textContent = '';
+
+        for (let i = 0; i < mensagens.length; i++) {
+            ulMensagens.textContent += mensagem(mensagens[i]);
+        }
+
+        scrollarAteAUltimaMensagem();
+    });
+}
+
+function mensagem(dados) {
+    let classeMensagem = '';
+    let destinatario = '';
+
+    if (dados.type === 'status') {
+        classeMensagem = 'entrada-saida'
+    }
+    if (dados.type === 'private-message') {
+        if (dados.to !== nomeDeUsuario &&
+            dados.to !== 'Todos' &&
+            dados.from !== nomeDeUsuario) {
+            return "";
+        }
+
+        classeMensagem = 'conversa-privada';
+        destinatario = `<span> para </span>
+        <strong>${dados.to}: </strong>`;
+    }
+
+    if (dados.type === 'message') {
+        classeMensagem = 'conversa-publica';
+        destinatario = `<span> para </span>
+        <strong>${dados.to}: </strong>`;
+    }
+
+    return `
+    <li class="${classeMensagem}" data-identifier="message">
+            <span class="horario">(${dados.time})</span>
+            <strong>${dados.from}</strong>
+            ${destinatario}
+            <span>${dados.text}</span>
+        </li>
+    `;
+}
+
+function scrollarAteAUltimaMensagem() {
+    const ulMensagens = document.querySelector('.mensagens-container');
+    const liUltimaMensagem = ulMensagens.lastElementChild;
+
+    if (ultimaMensagem !== liUltimaMensagem) {
+        liUltimaMensagem.scrollIntoView();
+        ultimaMensagem = liUltimaMensagem;
+    }
 }
